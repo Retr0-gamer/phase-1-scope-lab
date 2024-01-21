2d39864e3ebab44e20e52a432ecfe16203a7dbd9
+ <salimhetham@gmail.com>
2024-01-21 03:17:14 +0300

    practise

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = "bob"
+
+const leastFavoriteCustomer = "mike"
+
+var bestCustomer
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    return bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = "maybe bob"
+}
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = "mark"
+}
